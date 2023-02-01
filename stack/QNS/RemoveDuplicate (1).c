d507bf7070d72c0467a02b561e324bccecb69910
Sambit haldar <sambithaldar100@gmail.com>
2023-02-02 01:59:52 +0530

    first

---
+++ stack/QNS/RemoveDuplicate (1).c
@@ -0,0 +1,38 @@
+#include <stdio.h>  
+#include <conio.h>  
+int main ()  
+{   
+    int arr[20], i, j, k, size;  
+      
+    printf ("Array Size : ");  
+    scanf (" %d", &size);  
+      
+    printf (" \n Enter elements of an array:\n");  
+    for ( i = 0; i < size; i++)  
+    {  
+        scanf (" %d", &arr[i]);  
+    }  
+
+    for ( i = 0; i < size; i ++)  
+    {  
+        for ( j = i + 1; j < size; j++)  
+        {  
+            if ( arr[i] == arr[j])  
+            {  
+                 
+                for ( k = j; k < size - 1; k++)  
+                {  
+                    arr[k] = arr [k + 1];  
+                }  
+                size--;  
+                j--;      
+            }  
+        }  
+    } 
+    printf (" \n Array elements after deletion of the duplicate elements: "); 
+    for ( i = 0; i < size; i++)  
+    {  
+        printf (" %d \t", arr[i]);  
+    }  
+    return 0;  
+}  
